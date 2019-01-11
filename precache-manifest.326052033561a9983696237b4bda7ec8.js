1fb3524287c41a612d5fba2ed2b889e9d287b1d6
arncet <arnaud.cetoute@hotmail.fr>
2019-01-11 17:28:57 +0100

    Updates

---
+++ precache-manifest.326052033561a9983696237b4bda7ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04e54913055660810261",
+    "url": "/media-query-tester/static/js/runtime~main.04e54913.js"
+  },
+  {
+    "revision": "9c56bba374b7b9439d08",
+    "url": "/media-query-tester/static/js/main.9c56bba3.chunk.js"
+  },
+  {
+    "revision": "3e5d983933be408e96ba",
+    "url": "/media-query-tester/static/js/1.3e5d9839.chunk.js"
+  },
+  {
+    "revision": "3e5d983933be408e96ba",
+    "url": "/media-query-tester/static/css/1.7772521d.chunk.css"
+  },
+  {
+    "revision": "bfa94d0be6783c385127fb5116046fff",
+    "url": "/media-query-tester/index.html"
+  }
+];
